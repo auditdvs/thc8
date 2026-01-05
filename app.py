c90ce11976ba855928117b603897dc3bde4ee277
auditdvs <dvsaudit@gmail.com>
2026-01-05 11:26:53 +0700

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import io
 import uuid
 import time
 
-st.title("Aplikasi Pengolahan THC Link-2")
+st.title("Aplikasi Pengolahan THC Link-8")
 
 st.subheader("File yang dibutuhkan:")
 st.write("1. THC.csv")
